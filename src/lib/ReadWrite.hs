f6abbb9298b41d2f75d563d78cc515c5c0aab3be
LFDM <1986gh@gmail.com>
2017-10-31 17:43:07 +0100

    feat(read-write) Create a new todo list if it doesn't exist already

--- src/lib/ReadWrite.hs
+++ src/lib/ReadWrite.hs
@@ -1,2 +1,21 @@
 module ReadWrite (
+  createNewTodoFile
 ) where
+
+import System.Directory
+import System.FilePath
+import System.Environment as Env
+
+todoFileName = ".htd.yaml"
+
+createNewTodoFile :: IO Bool
+createNewTodoFile = do
+  cwd <- getCurrentDirectory
+  let f = joinPath [cwd, todoFileName]
+  doesFileExist f >>= tryCreateNewTodoFileAtPath f
+
+tryCreateNewTodoFileAtPath :: FilePath -> Bool -> IO Bool
+tryCreateNewTodoFileAtPath p True  = return False
+tryCreateNewTodoFileAtPath p False = writeFile p "" >> return True
+
+
