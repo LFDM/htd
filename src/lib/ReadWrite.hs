0030e33a36252ba50df1d997017110f2e8a6f8f1
LFDM <1986gh@gmail.com>
2017-10-31 18:59:43 +0100

    feat(read-write) Read the next possible todo file

--- src/lib/ReadWrite.hs
+++ src/lib/ReadWrite.hs
@@ -1,21 +1,55 @@
 module ReadWrite (
   createNewTodoFile
+, readTodoList
 ) where
 
+import Data.Maybe
 import System.Directory
 import System.FilePath
 import System.Environment as Env
+import Todos
+import Todo
+import qualified Data.Yaml as Y
+
+import qualified Data.ByteString.Char8 as BS
+
 
 todoFileName = ".htd.yaml"
+
+getTodoFileName :: String -> FilePath
+getTodoFileName dir = joinPath [dir, todoFileName]
 
 createNewTodoFile :: IO Bool
 createNewTodoFile = do
   cwd <- getCurrentDirectory
-  let f = joinPath [cwd, todoFileName]
+  let f = getTodoFileName cwd
   doesFileExist f >>= tryCreateNewTodoFileAtPath f
 
 tryCreateNewTodoFileAtPath :: FilePath -> Bool -> IO Bool
 tryCreateNewTodoFileAtPath p True  = return False
 tryCreateNewTodoFileAtPath p False = writeFile p "" >> return True
+
+readTodoList :: IO (Maybe Todos)
+readTodoList = do
+  ts <- getCurrentDirectory >>= findTodoFile >>= readTodoFile
+  print $ fromMaybe (newTodos [] "") ts
+  return Nothing
+
+findTodoFile :: String -> IO (Maybe FilePath)
+findTodoFile "" = return Nothing
+findTodoFile dir = doesFileExist f >>= returnOrRecurse
+  where f = getTodoFileName dir
+        returnOrRecurse True = return (Just f)
+        returnOrRecurse False = findTodoFile $ getParentDir dir
+
+readTodoFile :: Maybe FilePath -> IO (Maybe Todos)
+readTodoFile Nothing = return Nothing
+readTodoFile (Just p) = BS.readFile p >>= parse >>= createTodos
+  where parse f = return (Y.decode f :: Maybe [Todo])
+        createTodos Nothing = return Nothing
+        createTodos (Just todos) = return $ Just (newTodos todos p)
+
+getParentDir :: String -> String
+getParentDir = joinPath . init . splitDirectories
 
 
