e72cf91239771c9a9addb9a736e6bef8c64d650f
LFDM <1986gh@gmail.com>
2017-11-01 19:56:00 +0100

    feat(todos) Add update function

--- src/lib/Todos.hs
+++ src/lib/Todos.hs
@@ -4,19 +4,27 @@ module Todos where
 
 import Todo
 import Control.Lens
-import Data.Map
+import Data.Map as Map
 import Data.List as List
 
 data Todos = Todos { _todos :: Map String Todo
                    , _path :: FilePath
                    } deriving (Show)
 
+makeLenses ''Todos
+
 createTodosContainer :: FilePath -> [Todo] -> Todos
 createTodosContainer p ts = Todos{_todos=indexById ts, _path=p}
-  where indexById = fromList . List.map (\t -> (Todo.id t, t))
 
 getTodosList :: Todos -> [Todo]
 getTodosList = elems . _todos
 
-makeLenses ''Todos
+updateTodos :: Todos -> [Todo] -> Todos
+updateTodos c ts = todos .~ (Map.union newTodos oldTodos) $ c
+  where newTodos = indexById ts
+        oldTodos = _todos c
+
+
+indexById :: [Todo] -> Map String Todo
+indexById = fromList . List.map (\t -> (Todo.id t, t))
 
