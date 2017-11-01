50b5b91d4d248aff545aa061636767ef400123c4
LFDM <1986gh@gmail.com>
2017-11-01 19:40:35 +0100

    feat(todos) Use a Map to hold todos

--- src/lib/Todos.hs
+++ src/lib/Todos.hs
@@ -4,10 +4,19 @@ module Todos where
 
 import Todo
 import Control.Lens
+import Data.Map
+import Data.List as List
 
-data Todos = Todos { _todos :: [Todo]
+data Todos = Todos { _todos :: Map String Todo
                    , _path :: FilePath
                    } deriving (Show)
+
+createTodosContainer :: FilePath -> [Todo] -> Todos
+createTodosContainer p ts = Todos{_todos=indexById ts, _path=p}
+  where indexById = fromList . List.map (\t -> (Todo.id t, t))
+
+getTodosList :: Todos -> [Todo]
+getTodosList = elems . _todos
 
 makeLenses ''Todos
 
