adc23bb4af0d3666dfbb72137561a6f26a12495c
LFDM <1986gh@gmail.com>
2017-11-06 07:36:14 +0100

    feat(todo) Add create fn with id generation

--- src/lib/Todo.hs
+++ src/lib/Todo.hs
@@ -6,11 +6,13 @@ module Todo where
 
 import GHC.Generics
 
+
 import qualified Data.Yaml as Y
 import Data.Yaml (FromJSON(..), ToJSON(..))
 import qualified Data.ByteString.Char8 as BS
 import Control.Lens
 import Renderable
+import Util
 
 
 data TodoStatus = DONE | NOT_DONE | ARCHIVED
@@ -36,4 +38,9 @@ makeLenses ''Todo
 
 mark :: TodoStatus -> Todo -> Todo
 mark s = status .~ s
+
+createNewTodo :: IO Todo
+createNewTodo = do
+  nextId <- generateId
+  return Todo { _title="", _status=NOT_DONE, Todo.id=nextId}
 
