9fc20dcf5f89e720bc36d105f6897a5ef200a2f5
LFDM <1986gh@gmail.com>
2017-11-06 05:24:21 +0100

    feat(editor) Add fns to move to beginning and end in editor

--- src/ui/Editor.hs
+++ src/ui/Editor.hs
@@ -5,6 +5,8 @@ module Editor
 , createEditorView
 , handleEditorEvent
 , getEditorText
+, moveToStart
+, moveToEnd
 ) where
 
 import Brick.Types
@@ -16,6 +18,7 @@ import Brick.Widgets.Core
 import qualified Brick.Widgets.Edit as E
 import qualified Brick.Types as T
 import qualified Graphics.Vty as V
+import qualified Data.Text.Zipper as Z
 import Register
 
 type Editor = E.Editor String Name
@@ -36,3 +39,15 @@ handleEditorEvent = E.handleEditorEvent
 
 getEditorText :: Editor -> String
 getEditorText = unlines . E.getEditContents
+
+moveToStart :: Editor -> Editor
+moveToStart = move (0, 0)
+
+moveToEnd :: Editor -> Editor
+moveToEnd e= move (lastPos getLineLengths) e
+  where getLineLengths = view editorContentL e
+        lastPos [] = (0, 0)
+        lastPos xs = (length xs - 1, last xs)
+
+move :: (Int, Int) -> Editor -> Editor
+move pos = E.applyEdit (Z.moveCursor pos)
